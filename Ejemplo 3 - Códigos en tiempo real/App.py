54bf1f5028fe1fc0b2576774ba2c3b2096bb7f64
brayanpasa99 <brayanpasa99@hotmail.com>
2021-02-27 15:03:42 -0500

    Avances 27 de febrero de 2021 3:03 PM

--- Ejemplo 3 - Códigos en tiempo real/App.py
+++ Ejemplo 3 - Códigos en tiempo real/App.py
@@ -6,6 +6,8 @@ class Captura_codigos:
     
     def __init__(self):
         self.dato_coordenada={}
+        self.initial_positions={}
+        self.current_positions={}
 
     def read_barcodes(self, frame):
         barcodes = pyzbar.decode(frame, symbols=[ZBarSymbol.CODE128])
@@ -16,15 +18,62 @@ class Captura_codigos:
                 cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)   
                 if(self.dato_coordenada.has_key(barcode_text)):
                     self.dato_coordenada[barcode_text]=x+(w/2), y+(h/2)
+                    self.initial_positions = self.dato_coordenada
+                    self.ordena(self.dato_coordenada)
                 elif(not self.dato_coordenada.has_key(barcode_text)):
                     self.dato_coordenada[barcode_text]=x+(w/2), y+(h/2)
+                    self.initial_positions = self.dato_coordenada
+                    self.ordena(self.dato_coordenada)
                 else:
                     print("ERROR de ejecucion")
             return frame
         return frame
 
-    def ordena(self):
-        print("Ordena")
+    def ordena(self, coor): 
+        coor_x = {}
+        for key in coor:
+            coor_x[key]=coor[key][0]
+
+        valores = coor_x.values()
+        llaves = coor_x.keys()
+
+        ordenados = self.selectionSort(valores, llaves)
+        self.current_positions = dict(zip(ordenados[1], ordenados[0]))
+        
+        print("Current", self.current_positions)
+        print(ordenados[1])
+        print(ordenados[0])
+        print("Initial", self.initial_positions)
+        self.writeFile(ordenados[1])
+
+    def selectionSort(self, vList, llList):
+        for i in range(len(vList)):
+            least = i
+            for k in range(i+1, len(vList)):
+                if vList[k] < vList[least]:
+                    least = k
+                    
+            self.swap(vList, llList, least, i)
+
+            return [vList, llList]
+            
+    def swap(self, V, LL, x, y):
+        temp = [V[x], LL[x]]
+        [V[x], LL[x]] = [V[y], LL[y]]
+        [V[y], LL[y]] = temp
+
+    def writeFile(self, values):
+        #archivo-salida.py
+        # archivo-apendice.py
+
+        cadena = ""
+
+        for i in range(len(values)):
+            cadena = cadena +" "+ values[i]
+
+        f = open('data.txt','a')
+        f.write('\n' + cadena)
+        f.close()
 
     #def guarda_coordenadas(self, barcode_text, coordenadas)
 
